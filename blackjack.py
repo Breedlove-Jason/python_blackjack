3462ebf446e81751eaa905d6dff71ad295b09146
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-20 10:16:27 -0700

    generated cards and function to calculate value

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,10 @@
-############### Blackjack Project #####################
+from art import logo
+import random
 
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+############### Blackjack Project #####################
+print(logo)
+
+# Difficulty Expert 🤯: Only use Hint 1 to complete the project.
 
 ############### Our Blackjack House Rules #####################
 
@@ -17,3 +18,55 @@
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+# Create a list to store the deck of cards
+deck = []
+
+# Generate the deck of cards
+# for suit in suits:
+#     for rank in ranks:
+#         card = {
+#             'suit': suit,
+#             'rank': rank
+#         }
+#         deck.append(card)
+
+
+# Print the deck of cards
+# for card in deck:
+#     print(card['rank'], 'of', card['suit'])
+
+# Shuffle the deck of cards
+def shuffle_deck():
+    random.shuffle(ranks)
+
+
+shuffle_deck()
+
+
+def calc_card(card):
+    """Calculate the value of the card"""
+    if card == 'Jack' or card == 'Queen' or card == 'King':
+        return 10
+    elif card == 'Ace':
+        return 11
+    else:
+        return int(card)
+
+
+# Deal a card from the deck
+# Player Cards
+player_card_1 = calc_card(random.choice(ranks))
+player_card_2 = calc_card(random.choice(ranks))
+print(f"Player's cards: {player_card_1}, {player_card_2}")
+total_player_cards = player_card_1 + player_card_2
+print(f"Total: {total_player_cards}")
+
+# Computer Cards
+computer_card_1 = calc_card(random.choice(ranks))
+computer_card_2 = calc_card(random.choice(ranks))
+total_computer_cards = computer_card_1 + computer_card_2
+print(f"Computer's cards: {computer_card_1}, {computer_card_2}")
+print(f"Total: {total_computer_cards}")
