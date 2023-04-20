e3d53501cf1caf9af165d299f4c684aab95da137
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-20 15:09:48 -0700

    revise code to allow the ace in the deck as 1 or 11

--- blackjack.py
+++ blackjack.py
@@ -24,6 +24,7 @@ ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 
 # Create a list to store the deck of cards
 deck = []
 
+
 # Generate the deck of cards
 # for suit in suits:
 #     for rank in ranks:
@@ -50,23 +51,79 @@ def calc_card(card):
     """Calculate the value of the card"""
     if card == 'Jack' or card == 'Queen' or card == 'King':
         return 10
-    elif card == 'Ace':
-        return 11
+    if card == 'Ace':
+        ace_input = input("Ace Drawn! Do you want to use it as 1 or 11? ")
+        if ace_input == '1':
+            return 1
+        if ace_input == '11':
+            return 11
     else:
         return int(card)
 
 
-# Deal a card from the deck
+def hit():
+    """Deal a card from the deck"""
+    return calc_card(random.choice(ranks))
+
+
 # Player Cards
 player_card_1 = calc_card(random.choice(ranks))
+print(f"Player Draws: {player_card_1}")
 player_card_2 = calc_card(random.choice(ranks))
-print(f"Player's cards: {player_card_1}, {player_card_2}")
-total_player_cards = player_card_1 + player_card_2
-print(f"Total: {total_player_cards}")
+print(f"Player Draws: {player_card_2}")
+# print(f"Player Draws: {player_card_1}, {player_card_2}")
+player_total = player_card_1 + player_card_2
 
 # Computer Cards
 computer_card_1 = calc_card(random.choice(ranks))
+print(f"Computer Draws: {computer_card_1}")
 computer_card_2 = calc_card(random.choice(ranks))
-total_computer_cards = computer_card_1 + computer_card_2
-print(f"Computer's cards: {computer_card_1}, {computer_card_2}")
-print(f"Total: {total_computer_cards}")
+print(f"Computer Draws: ##")
+# print(f"Computer Draws: {computer_card_1}, {computer_card_2}")
+computer_total = computer_card_1 + computer_card_2
+
+user_hit = True
+computer_hit = True
+while user_hit:
+    user_input = input("Hit or Stand? 'h/s' ").lower()
+
+    if user_input == 'h':
+        hit_me = hit()
+        player_total += hit_me
+        print(f"You draw: {hit_me} and your total is {player_total} ")
+        if player_total > 21:
+            print("You Bust!")
+            print("Computer Wins!")
+            computer_hit = False
+            break
+            # user_hit = False
+        elif player_total == 21:
+            print("Player Gets Blackjack!")
+            print("You Win!")
+            computer_hit = False
+            user_hit = False
+    if user_input == 's':
+        print("You stand!")
+        print(player_total)
+        print(f"Computer Reveals: {computer_card_2} and their total is {computer_total} ")
+        user_hit = False
+
+while computer_hit:
+    if computer_total < 17:
+        computer_hit = hit()
+        computer_total += computer_hit
+        print(f"Computer draws: {computer_hit} and their total is {computer_total} ")
+    elif computer_total > 21:
+        print("Computer Busts!")
+        print("You Win!")
+        break
+    elif computer_total == 21:
+        print("Computer Gets Blackjack!")
+        print("Computer Wins!")
+        break
+    elif computer_total > player_total:
+        print("Computer Wins!")
+        break
+    elif computer_total == player_total:
+        print("Draw!")
+        break
